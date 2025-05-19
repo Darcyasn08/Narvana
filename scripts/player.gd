62b602ab851bf63b4044e56587496973ab5f1577
Júlia <heylavenderbee@gmail.com>
2025-05-19 19:11:27 -0300

    arquivos iniciais e alguns placeholders

---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+# Nota: as teclas de movimentação e de esc já estão definidas no projeto!
+
+func _ready() -> void:
+	pass
+	#coloque aqui as funções que irão rodar quando a cena iniciar
+
+func _physics_process(delta: float) -> void:
+	pass
+	#coloque aqui as funções que serão rodadas por frame
+
+# função da movimentação do player
+func move():
+	pass
