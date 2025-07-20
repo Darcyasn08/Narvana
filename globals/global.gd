fc3996f5bb4ffc4e55f7e55680449357f563c423
Lucassn08 <132584182+Lucassn08@users.noreply.github.com>
2025-07-20 14:23:11 -0300

    second_level_start_pt1

--- globals/global.gd
+++ globals/global.gd
@@ -1,90 +1,35 @@
 extends Node
 
-var has_started_game: bool = false
-
 var player_can_move: bool = true
-
-var dust_damage: float= 1500.0
+var player_damage : float = 200.0
+var dust_damage :float = 1500.0
 var player_can_attack: bool = true
 var player_base_pos: Vector3 = Vector3(0,5,0)
 
-var base_player_health: int = 6
-var base_player_damage: float = 200.0
-var base_player_speed: float = 8.0
-
 var player_health: int = 6
-var player_damage: float = 200.0
-var player_speed: float = 8.0
-
-var plus_player_health: int
-var plus_player_damage: float
-var plus_player_speed: float
-
 var house_health: int = 5000
 
-var enemies: Array = [
-	["res://scenes/enemies/coin.tscn","res://scenes/enemies/pig_bank.tscn","res://scenes/enemies/car.tscn", "res://scenes/enemies/pearl_collar.tscn", "res://scenes/enemies/house.tscn"], #uma fase
-	[],
-	[]
+
+
+var enemies = [
+	["res://scenes/coin.tscn","res://scenes/pig_bank.tscn"] #uma fase
 ]
 
-var dead_enemies_first_level: Array = [
+var dead_enemies_first_level = [
 	[0, 0],
-	[0, 0],
-	[0, 0],
-	[0, 0],
+	[0, 0]
 ]
 
 var current_weapon: String = "bat"
 
 var current_room: int = 0
 var current_world: int
-enum worlds {NORMAL, FIRST_LEVEL, SECOND_LEVEL}
+enum worlds {NORMAL, FIRST_LEVEL}
 
 var completed_levels: Dictionary = {
 	"first_level": false,
 	"second_level": false,
 	"third_level": false
-}
-
-var inventory: Dictionary = {
-	"current_weapon": current_weapon,
-	"current_spell": "",
-	"items": {
-		"photo": {
-			"player_has": true,
-			"icon": "res://icon.svg",
-			"name": "Foto da banda",
-			"desc": "Dessa foto, vem muitas memórias, e uma certa vontade de continuar (+ataque)",
-			"buff": {
-				"damage": 50,
-				"health": 0,
-				"speed": 0,
-			},
-		},
-		"teddy": {
-			"player_has": true,
-			"icon": "res://UI/inventory/teddy-bear.png",
-			"name": "Ursinho de pelúcia antigo",
-			"desc": "Algo dele te traz um conforto muito grande (+vida)",
-			"buff": {
-				"damage": 0,
-				"health": 1,
-				"speed": 0,
-			},
-		},
-		"coffee": {
-			"player_has": true,
-			"icon": "res://UI/inventory/coffee-cup.png",
-			"name": "Copo de café",
-			"desc": "É sempre bom um café pela manhã (+velocidade)",
-			"buff": {
-				"damage": 0,
-				"health": 0,
-				"speed": 2,
-			},
-		}
-	}
 }
 
 var cutscenes: Dictionary = {
@@ -100,7 +45,7 @@ var dialogs: Dictionary = {
 			"is_first_time": true,
 			"first_dialog": {
 				"text": "hi",
-				"options": {},
+				"options": {}
 			},
 			"middle": {
 				0: {
@@ -144,130 +89,10 @@ var dialogs: Dictionary = {
 				"text": "Até logo, rapaz",
 				"options": {}
 			}
-		}
-	},
-	"jellyfish": {
-		"dialog_tree": {
-			"is_first_time": true,
-			"first_dialog": {
-				"text": "hi",
-				"options": {}
-			},
-			"middle": {
-				0: {
-					"text": "Oi, eu sou o Anderson",
-					"options": {}
-				},
-				1: {
-					"text": "Dizem que eu sou o cara mais maneiro daqui... Você acredita nisso? :D", 
-					"options": {}
-				},
-				2: {
-					"text": "Bom, eu tenho negócios a fazer, te vejo alguma hora",
-					"options": {}
-				},
-			},
-			"quest": {
-				#none/ongoing/done - vai checar qual tá cada vez que o dialogo for acionado
-				"status": "none",
-				"id": "secret_stash",
-				"text": "(fazer um texto aqui alguma hora)",
-				"options": {}
-			},
-			"exit": {
-				"text": "Te vejo mais tarde",
-				"options": {}
-			}
-		}
-	},
-		
-	"grandma": {
-		"dialog_tree": {
-			"is_first_time": true,
-			"first_dialog": {
-				"text": "hi",
-				"options": {}
-			},
-			"middle": {
-				0: {
-					"text": "Oi, eu sou o Anderson",
-					"options": {}
-				},
-				1: {
-					"text": "Dizem que eu sou o cara mais maneiro daqui... Você acredita nisso? :D", 
-					"options": {}
-				},
-				2: {
-					"text": "Bom, eu tenho negócios a fazer, te vejo alguma hora",
-					"options": {}
-				},
-			},
-			"quest": {
-				#none/ongoing/done - vai checar qual tá cada vez que o dialogo for acionado
-				"status": "none",
-				"id": "secret_stash",
-				"text": "(fazer um texto aqui alguma hora)",
-				"options": {}
-			},
-			"exit": {
-				"text": "Te vejo mais tarde",
-				"options": {}
-			}
-		}
-	},
-	
-	"master": {
-		"is_first_time": true,
-		"dialog_tree": {
-			"middle": {
-				0: {
-					"text": "Olá garoto, vejo que você é bem jovem",
-					"options": {},
-				},
-				1: {
-					"text": "E pelas suas vestimentas, é de fora, não é?",
-					"options": {},
-				},
-				2: {
-					"text": "Hmm... também tem um ar de tristeza envolta de ti",
-					"options": {},
-				},
-				3: {
-					"text": "Venha, posso ajudar a sua jovem alma a curar todas as suas tristezas",
-					"options": {
-						0: {
-							"text": "Ok",
-							"ignite": "function",
-						},
-						1: {
-							"text": "Ok",
-							"ignite": "function",
-						},
-					},
-				},
-				4: {
-					"text": "Após se aproximar no portal, entre nele usando [e] e vou te ajudar nessa jornada",
-					"options": {},
-				},
-			},
-			"function": {
-				"status": "none",
-				"text": "(clica [e] pra continuar, não pensei nesse dialogo ainda)",
-				"id": "open_first_level_portal",
-				"options": {},
-			}
-		}
-	},
-	
-	"first_level_master": {
-		"is_first_time": true,
-	},
-	"second_level_master": {
-		"is_first_time": true,
-	},
-	"third_level_master": {
-		"is_first_time": true,
-	},
+		},
+		"jellyfish": {},
+		"grandma": {},
+	}
 }
 
 var quests: Dictionary = {
@@ -284,5 +109,55 @@ var quests: Dictionary = {
 			"desc": "give grandma some flowers",
 			"item_to_give": "flower bouquet"
 		}
+	}
+}
+
+var dialogues: Dictionary = {
+	"crab": {
+		0: [
+			"hello",
+			"i am crab",
+			"how are you?",
+			"i'm fine; i'm sad :(",
+			"ok :D"
+		],
+	},
+	
+	"master": {
+		0: [
+			"hello little one",
+			"i wonder what brings you here...",
+			"oh!",
+			"have you just come here to escape your old damn life?"
+		],
+	},
+	
+	"jellyfish": {
+		0: [
+			"vamo rir vamo rir",
+			"vamo rir, daniel, vamo rir",
+			"muahahaHAHAHAHAH"
+		],
+	}
+}
+
+var crab_diag: Dictionary = {
+	"name": "crab junior",
+	"missions": {
+		0: [
+			"your first missions is..",
+			"give me your phone"
+		]
+	},
+	
+	"daily": {
+		0: [
+			"hey soul sister",
+			"i dont care"
+		],
+		1: [
+			"my godness!",
+			"youre a bitch."
+		]
 	}
 }
--- globals/signal_bus.gd
+++ globals/signal_bus.gd
@@ -10,9 +10,6 @@ signal on_first_level_entered()
 signal on_room_completed()
 signal on_start_room()
 
-signal on_item_list_updated()
-signal on_item_removed()
-
-signal on_start_dialog_function()
-
 signal on_player_health_changed()
+
+signal on_blush_hit()
