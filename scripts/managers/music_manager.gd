e67fe8d52e663a6e74dc17c7c0a73a6a1ddd2d75
Júlia <heylavenderbee@gmail.com>
2025-08-28 05:41:37 -0300

    melhora moldes boss casa e banda

--- scripts/managers/music_manager.gd
+++ scripts/managers/music_manager.gd
@@ -6,12 +6,13 @@ var second_level_array: Array = []
 
 var song_index: int = 0
 
-@onready var pause: AudioStreamPlayer3D = $first_level/pause
+@onready var first_level_pause: AudioStreamPlayer3D = $first_level/pause
+@onready var normal_world_pause: AudioStreamPlayer3D = $normal_world/pause
 
 func _ready() -> void:
 	SignalBus.on_game_paused.connect(start_pause_song)
-	for child in $first_level/main.get_children():
-		first_level_array.append(child)
+	for child in $normal_world/main.get_children():
+		normal_world_array.append(child)
 	for child in $first_level/main.get_children():
 		first_level_array.append(child)
 	for child in $second_level/main.get_children():
@@ -20,17 +21,22 @@ func _ready() -> void:
 
 func start_pause_song(is_paused: bool) -> void:
 	if is_paused:
-		pause.play()
+		match Global.current_world:
+			Global.worlds.FIRST_LEVEL:
+				first_level_pause.play()
+			Global.worlds.NORMAL:
+				normal_world_pause.play()
 	else:
-		pause.stop()
+		normal_world_pause.stop()
+		first_level_pause.stop()
 
 
 func _on_timer_timeout() -> void:
 	match Global.current_world:
 		Global.worlds.NORMAL:
-			first_level_array[song_index].play()
+			normal_world_array[song_index].play()
 			song_index += 1
-			if song_index > first_level_array.size() - 1:
+			if song_index > normal_world_array.size() - 1:
 				$Timer.stop()
 			else:
 				$Timer.start()
